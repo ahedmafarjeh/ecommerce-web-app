d49665aa132074eee6c1ab1605e4a03d39189799
ahedmafarjeh <ahedmafarjeh10@gmail.com>
2025-01-19 19:50:05 +0200

    updated v1

--- src/components/user/navbar/CustomNavbar.jsx
+++ src/components/user/navbar/CustomNavbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import style from './navbarStyle.module.css'
 
 export default function CustomNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={style.darknav}>
       <Container>
-        <Navbar.Brand href="#home">Ahed-Shop</Navbar.Brand>
+        <Navbar.Brand className='text-light' href="#home">Ahed-Shop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to={'/login'}>Login</Nav.Link>
-            <Nav.Link as={Link} to={'/register'}>Register</Nav.Link>
+            <Nav.Link as={Link}  to={'/login'}>Login</Nav.Link>
+            <Nav.Link as={Link}  to={'/register'}>Register</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
