4711823a380608d362b0e21c1535c8a5dfacef1a
ahedmafarjeh <ahedmafarjeh10@gmail.com>
2025-01-24 21:38:08 +0200

    update: create category and products pages

--- src/components/useFetch/useFetch.jsx
+++ src/components/useFetch/useFetch.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import Loading from '../Loading/Loading';
 export default function useFetch(url) {
     const [data, setData] = useState([{}]);
     const [error, setError] = useState('');
